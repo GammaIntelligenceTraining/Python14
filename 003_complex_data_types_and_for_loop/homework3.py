e12b82ca507de4ae2527db57b4961868be8cd60d
nkzubrilov <nkzubrilov@gmail.com>
2024-01-23 22:07:26 +0200

    Lesson 3 added

---
+++ 003_complex_data_types_and_for_loop/homework3.py
@@ -0,0 +1,25 @@
+# You have given a Python list. Write a code to find value 20 in the list, and if it is present,
+# replace it with 200. Only update the first occurrence of an item.
+list1 = [5, 10, 15, 20, 25, 50, 20]
+
+# Swap first and last elements in the given list
+lst = ['first', 'a', 'b', 'c', 'd', 'last']
+
+# Concatenate two lists to get the new one: ['Hello Dear', 'Hello Sir', 'take Dear', 'take Sir']
+lst1 = ["Hello ", "take "]
+lst2 = ["Dear", "Sir"]
+
+# Using for loop calculate the factorial of 11.
+# Factorial of 11 (11!) is a product of 1 x 2 x 3 ... x 11
+
+# You have a text. Write a program to return a list of words of this text
+# cleared of any punctuation, and capitalized.
+text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam consequat molestie sem, eu lacinia nisi hendrerit et. Maecenas risus ex, cursus ac tincidunt eu, porttitor ac diam. Morbi posuere laoreet ante, sed aliquam risus laoreet vel. Donec fringilla sollicitudin metus, quis finibus odio tincidunt quis. In a neque semper, sodales enim bibendum, sodales tellus. Sed eu efficitur metus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Suspendisse placerat tincidunt lectus, id commodo lacus sollicitudin ut. Aliquam sed consectetur sem. Nullam euismod magna vitae augue tristique interdum ac id dui. Nam neque libero, lacinia ut purus.'
+
+# Add a list of elements to a set
+sample_set = {"Yellow", "Orange", "Black"}
+sample_list = ["Blue", "Green", "Red"]
+
+# Return a new set of identical items from two sets
+set1 = {10, 20, 30, 40, 50}
+set2 = {30, 40, 50, 60, 70}
