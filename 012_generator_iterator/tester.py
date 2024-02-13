bab49d8b361742c49940f0b44e1d6a571088c062
Roman Kutselepa <roman.kutselepa@gmail.com>
2024-02-13 20:57:03 +0200

    Itertools

--- 012_generator_iterator/tester.py
+++ 012_generator_iterator/tester.py
@@ -1,28 +1,12 @@
-def squares_generator(start, end):
-    for num in range(start, end + 1):
-        yield num ** 2
-
-def infinite_counter(start):
-    while True:
-        start += 1
-        yield start - 1
-
-# y = infinite_counter(0)
-# print(next(y))
-# print(next(y))
-# print(next(y))
-# print(next(y))
-# for i in y:
-#     print(i)
-
-
-# x = squares_generator(1, 100)
+# def squares(start, stop):
+#     for i in range(start, stop):
+#         yield i ** 2
+#
+# x = squares(0, 10)
 #
 # print(next(x))
 # print(next(x))
 # print(next(x))
 # print(next(x))
 # print(next(x))
-#
-# for i in x:
-#     print(i)
+# print(next(x))
--- 013_itertools_module/tester.py
+++ 013_itertools_module/tester.py
@@ -1,100 +1,44 @@
 import itertools
 
-# counter = itertools.count()
-#
-# print(list(zip([10, 20, 30, 40], counter)))
-#
-# data = [100, 200, 300, 400, 500]
-# data2 = [5, 6, 7, 8, 9, 10, 11, 12, 13]
-
-# print(list(itertools.zip_longest(data, data2)))
-
-# counter = itertools.count(start=1.5, step=-0.5)
-# counter = itertools.cycle(['on', 'off'])
-# print(list(zip(data2, counter)))
-# counter = itertools.repeat(3, times=4)
-# print(list(zip(data2, counter)))
-# print(next(counter))
-# print(next(counter))
-# print(next(counter))
-# print(next(counter))
-# print(next(counter))
-
-# letters = ['a', 'b', 'c', 'd']
-# numbers = [1, 2, 3, 4]
-# numbers2 = [4, 5, 4, 3, 2, 1, 0, 4, 9, 2, 0, 45]
-# selectors = [True, False, False, True]
-# names = ['Jack', 'Mary']
-#
-# # NO ORDER, NO REPEAT
-# # result = itertools.combinations(range(10), 4)
-# # ORDER, NO REPEAT
-# # result = itertools.permutations(letters, 2)
-# # ORDER, REPEAT
-# # result = itertools.product(letters, repeat=2)
-# # NO ORDER, REPEAT
-# # result = itertools.combinations_with_replacement(letters, 4)
-#
-# # for c in result:
-# #     print(c)
-#
-# # result = itertools.compress(letters, numbers)
-# # print(list(result))
-# #
-# # result = itertools.filterfalse(lambda x: x > 2, numbers2)
-# # print(list(result))
-# # result2 = filter(lambda x: x > 2, numbers2)
-# # print(list(result2))
-#
-# # result = itertools.dropwhile(lambda x: x > 2, numbers2)
-# # result = itertools.takewhile(lambda x: x > 2, numbers2)
-# # print(list(result))
-#
-# result = itertools.accumulate(numbers2)
-# print(list(result))
+def get_city(person):
+    return person['city']
 
 people = [
     {
-        'name': 'John Smith',
+        'name': 'Jack',
         'city': 'Berlin'
     },
     {
-        'name': 'Mary Gold',
+        'name': 'Mary',
         'city': 'Berlin'
     },
     {
-        'name': 'Taavi Tamm',
+        'name': 'Sarah',
         'city': 'Berlin'
     },
     {
-        'name': 'Piere Cardin',
+        'name': 'Bob',
         'city': 'Tallinn'
     },
     {
-        'name': 'Jack Rock',
+        'name': 'Jessica',
         'city': 'Tallinn'
     },
     {
-        'name': 'Lee Hong',
+        'name': 'Simon',
         'city': 'Tallinn'
     },
     {
-        'name': 'Abdul Faruh',
-        'city': 'Dubai'
+        'name': 'Roger',
+        'city': 'Berlin'
     },
-    {
-        'name': 'Mary Pierce',
-        'city': 'Dubai'
-    },
-    {
-        'name': 'Lee Hong',
-        'city': 'Tallinn'
-    }
 ]
+people.sort(key=get_city)
+result = itertools.groupby(people, get_city)
 
-people.sort(key=lambda x: x['city'])
-result = itertools.groupby(people, lambda x: x['city'])
+copy1, copy2 = itertools.tee(result)
 
-for key, group in result:
-    # print(key, group)
-    print(list(group))
+for key, group in copy1:
+    print(key)
+    for person in group:
+        print(person)
